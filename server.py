f0f0ded7cc3ede72bcaa6ebc98ea2c6b5cb5cbcc
Robert Fornos <rf2830@columbia.edu>
2025-04-23 10:23:33 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+# Chidi Azuike       - ca2970
+# Robert Fornos      - rf2830
+# Shreya Somayajula  - svs2137
+# Vaibhav Sourirajan - vs2787
+
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+# GLOBAL VARIABLES
+
+# ROUTES
+
+# Home
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+# Learn
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+# Quiz
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+# AJAX FUNCTIONS
+
+if __name__ == '__main__':
+   app.run(debug = True, port=5001)
